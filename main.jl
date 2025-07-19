5f67e34c0d1deb05d6a63d43bf0cc6685eaa3d9e
ryanguo13 <stephanietclayton9746@gmail.com>
2025-07-19 15:39:45 +0800

    719 update

--- main.jl
+++ main.jl
@@ -5,12 +5,12 @@ rn = @reaction_network begin
     (k1, k₋1), A <--> B
     (k2, k₋2), B <--> C
     (k3, k₋3), C <--> D
-    k_ex, D --> ∅
+    # k_ex, D --> ∅
 end
 
 u0 = [:A => 10.0, :B => 0.0, :C => 0.0, :D => 0.0]
 tspan = (0.0, 10.0)
-p = [45.0, 36.0, 10.0, 1.0, 21.0, 12.0, 1.0]  # [k1, k₋1, k2, k₋2, k3, k₋3, k_ex]
+p = [45.0, 36.0, 10.0, 1.0, 21.0, 12.0]  # [k1, k₋1, k2, k₋2, k3, k₋3, k_ex]
 
 
 prob = ODEProblem(rn, u0, tspan, p)
@@ -21,7 +21,7 @@ plot(sol, xlabel="Time (min)", ylabel="Concentration",
      title="Species Concentrations vs Time", 
      label=["[A]" "[B]" "[C]" "[D]"])
 
-savefig("concentration.svg")
+savefig("concentration.png")
 
 t = sol.t
 A_vals = sol[:A]; B_vals = sol[:B]; C_vals = sol[:C]; D_vals = sol[:D]
@@ -34,4 +34,4 @@ plot(t, flux_AB, label="A⇄B", xlabel="Time (min)", ylabel="Net Flux",
 plot!(t, flux_BC, label="B⇄C")
 plot!(t, flux_CD, label="C⇄D")
 
-savefig("flux.svg")
+savefig("flux.png")
